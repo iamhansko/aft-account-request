65a6887ab3c53bfb76f2891be633d634688ee705
iamhansko <iamhansko@kakao.com>
2025-09-01 23:09:24 +0900

    init

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ module "aft_account_request" {
     SSOUserEmail              = "user${count.index}@example.com"
     SSOUserFirstName          = "AWS"
     SSOUserLastName           = "User${count.index}"
-    ManagedOrganizationalUnit = "Sandbox" # Sandbox OU
+    ManagedOrganizationalUnit = "Sandbox"
   }
 
   change_management_parameters = {
