8bddacf804172f29cb47333031824a13e3982c65
iamhansko <iamhansko@kakao.com>
2025-09-01 22:08:46 +0900

    init

---
+++ main.tf
@@ -0,0 +1,24 @@
+module "aft_account_request" {
+  source = "./modules/aft-account-request"
+  count = 7
+
+  control_tower_parameters = {
+    AccountName               = "user${count.index+1}"
+    AccountEmail              = "user${count.index}@example.com"
+    SSOUserEmail              = "user${count.index}@example.com"
+    SSOUserFirstName          = "AWS"
+    SSOUserLastName           = "User${count.index}"
+    ManagedOrganizationalUnit = "Root" # Root OU
+  }
+
+  account_tags = {
+    ManagedBy = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Terraform"
+    change_reason       = "Account provisioning"
+  }
+
+  account_customizations_name = "user${count.index}-custom"
+}
